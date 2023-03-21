2e14fa1c603f70a77a80de2ba3e37c8dd73ca57d
hepno <me@hepno.dev>
2023-03-22 00:19:06 +0100

    init

---
+++ src/main/java/dev/hepno/voidworld/VoidWorld.java
@@ -0,0 +1,17 @@
+package dev.hepno.voidworld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VoidWorld extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
